2b3db863c9707db0cef1186ccf69b43ca3029fcb
Goose <cybergoose13@gmail.com>
2025-07-02 20:02:32 -0700

    added guess the number in functions

--- functions.py
+++ functions.py
@@ -25,6 +25,17 @@ def magic_eight(answerNumber):
     else:
         return 'Sorry, I dont understand the words coming out of your mouth.'
     
+# Guess the number game: i know my casing isnt consistent
+# But neither is my brain....
+def guessTheNumber():
+    myNumber = random.randint(1, 20)
+    guessedNumber = input('Guess a number between 1 & 20')
+    if(guessTheNumber == myNumber):
+        return 'You have choosen wisely.'
+    return 'You have choosen poorly.'
+
 r = random.randint(1,9)
 fortune = magic_eight(r)
 print(fortune)
+
+print(guessTheNumber())
